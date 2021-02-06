aa38277c33aacab81f223d4971424d967b55402e
Taksayapa Tangwong <tuksayapa.t@kkumail.com>
2021-02-06 14:25:38 +0700

    update

--- week3/countloop.py
+++
@@ -1,9 +0,0 @@
-print("กรุณากรอกจำนวนครั้งการรับค่า ")
-num=int(input(""))
-i=0
-sum=0
-while(i < num) :
-    number=int(input("กรอกตัวเลข"))
-    i+=1
-    sum = sum + number
-print("ผลรวมค่าที่รับมาทั้งหมด :"+str(sum))
--- week3/test3.2.py
+++ week3/test3.2.py
@@ -1,24 +1,9 @@
-print("-"*30)
-student_list=int(input("Please enter student:"))
-print("-"*30)
-a=1
-total_list=[0,0,0,0,0,0]
-score_list=["90-100:","80-89:","70-70:","60-69:","50-59:","0-49:"]
-for x in range(0,student_list):
-    i =int(input("Please enter score"+str(a)+":"))
-    a += 1
-    if i <= 100 and i >= 90 :
-        total_list [0] +=1
-    elif i <= 90 and i >= 80 :
-        total_list [1] +=1
-    elif i <= 80 and i >= 70 :
-        total_list [2] +=1
-    elif i <= 70 and i >= 60 :
-        total_list [3] +=1
-    elif i <= 60 and i >= 50 :
-        total_list [4] +=1
-    elif i <= 50 and i >= 0 :
-        total_list [5] +=1
-for x in range(0,6):
-    print(score_list [x],"*"*total_list [x])
-
+print("กรุณากรอกจำนวนครั้งการรับค่า ")
+num=int(input(""))
+i=0
+sum=0
+while(i < num) :
+    number=int(input("กรอกตัวเลข"))
+    i+=1
+    sum = sum + number
+print("ผลรวมค่าที่รับมาทั้งหมด :"+str(sum))
