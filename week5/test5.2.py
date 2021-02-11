1278db9e035d6747f383bc3c59826f06d6c52dc1
Taksayapa Tangwong <tuksayapa.t@kkumail.com>
2021-02-11 21:20:30 +0700

    update

--- week5/test5.2.py
+++ week5/test5.2.py
@@ -1,2 +1,40 @@
 #ใบงานที่5.2
-print("***************หลินโภชนา****************")
+num = 0
+goods_ = []
+raca_ = []
+class shop:
+    def __init__(self,goods,raca):
+        self.goods = goods
+        self.raca = raca
+    def showshop(self):
+        print(i+1,".",self.goods,self.raca,"บาท")
+    def show():
+        print("----------------hifiveshop---------------")
+        print("แสดงรายการสินค้า [a]\nเพิ่มรายการสินค้า[s]\nออกจากระบบ[x]")
+
+while True:
+    shop.show()
+    menu = input("กรุณาเลือกคำสั่ง:\t")
+    if menu == 'a':
+        for i in range(num):
+            x = shop(goods_[i],raca_[i])
+            x.showshop()
+    if menu == 's':
+        while True:
+            print("เพิ่มรายการสินค้าหากต้องการยกเลิกกรอก exit")
+            g = input("เพิ่มชื่อสินค้า:\t")
+            if g == 'exit':
+                break
+            else:
+                goods_.append(g)
+                r = input("เพิ่มราคาของ"+g+":\t")
+                raca_.append(r)
+                print("ทำรายการเสร็จสิน")
+                num +=1
+                yn = input("ต้องการเพิ่มสินค้าอีกหรือไม่(y/n):")
+                if yn == 'n':
+                    break
+    if menu == 'x':
+        yn = input("ต้องการเพิ่มสินค้าอีกหรือไม่(y/n):")
+        if yn == 'y':
+            break
